6b2da594913232b8a42fee445b082c967b4919bb
michaelcummins1 <mjcrocks1@gmail.com>
2023-04-21 10:33:17 -0400

    Added PickPersonFragment.kt and edited UI

--- app/src/main/java/com/example/finalproject/DisplayEventsFragment.kt
+++ app/src/main/java/com/example/finalproject/DisplayEventsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class DisplayEventsFragment : Fragment() {
 
@@ -12,7 +15,12 @@ class DisplayEventsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        //Just to test if Firebase works will probably have to be used in ViewModel.kt
+        lateinit var dbRef : DatabaseReference
+        dbRef = Firebase.database.reference
+
+
         return inflater.inflate(R.layout.fragment_display_events, container, false)
     }
 }
